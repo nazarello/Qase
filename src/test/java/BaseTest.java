ab7c75bc2211394058f4b388583511431313ec06
radzi <radzivon.kanash@gmail.com>
2022-01-29 14:21:39 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
+import utils.PropertyReader;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class BaseTest {
+String email, password;
+
+    @BeforeMethod
+    public void setUp() {
+        //Configuration.headless = true;
+        Configuration.baseUrl = System.getenv().getOrDefault("QASE_URL", PropertyReader.getProperty("qase.url"));
+        email = System.getenv().getOrDefault("QASE_EMAIL", PropertyReader.getProperty("qase.email"));
+        password = System.getenv().getOrDefault("QASE_PASSWORD", PropertyReader.getProperty("qase.password"));
+        Configuration.browser = "chrome";
+        //   Configuration.clickViaJs = true;
+        Configuration.savePageSource = false;
+        Configuration.timeout = 10000;
+
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        // chromeOptions.addArguments("headless");
+//        Configuration.browserCapabilities = chromeOptions;
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        getWebDriver().quit();
+
+       /* WebDriver driver = new ChromeDriver();
+        setWebDriver(driver);*/
+    }
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void login() {
+        open("/login");
+        $("#inputEmail").sendKeys("radzivon.kanash@gmail.com");
+        $(By.xpath("//*[@id='inputPassword']")).setValue("GranadaGranada1!");
+        $("#btnLogin").click();
+        $(By.id("createButton")).shouldBe(visible);
+    }
+
+    @Test
+    public void login2() {
+        open("login ");
+        $("#inputEmail").sendKeys("radzivon.kanash@gmail.com");
+        $(By.xpath("//*[@id='inputPassword']")).setValue("GranadaGranada1!");
+        $("#btnLogin").click();
+        $(By.id("createButton")).shouldBe(visible);
+    }
+}
